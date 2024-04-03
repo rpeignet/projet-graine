8a08858ce2769550146a75f2ebc211ab8c96d47e
Yannick Boyer <yannickboyer25@gmail.com>
2024-04-03 14:42:57 +0200

    Ajout vue sachets_fournisseurs

--- projet-graine/src/main/java/fr/projetgraine/service/SachetService.java
+++ projet-graine/src/main/java/fr/projetgraine/service/SachetService.java
@@ -1,2 +1,12 @@
-package fr.projetgraine.service.impl;public interface SachetService {
+package fr.projetgraine.service;
+
+import fr.projetgraine.business.Sachet;
+import fr.projetgraine.business.TypeDeGraine;
+
+import java.util.List;
+
+public interface SachetService {
+    public List<Sachet> findAll();
+    public boolean addSachet(Sachet sachet);
+    public boolean deleteSachet(Sachet sachet);
 }
