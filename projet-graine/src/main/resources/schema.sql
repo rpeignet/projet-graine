e7e2c230d7ae94025a661411333313ca210db09f
Yannick Boyer <yannickboyer25@gmail.com>
2024-04-03 11:03:52 +0200

    Delete seeds table

--- projet-graine/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS seeds (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    seedName VARCHAR(255),
-    minPlantWeek INT,
-    maxPlantWeek INT,
-    spaceBtwLine INT,
-    spaceBtwFeet INT
-);
