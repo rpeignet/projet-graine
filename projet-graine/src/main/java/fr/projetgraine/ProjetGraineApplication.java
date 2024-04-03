d7b82303928c5c073d688d7c0fe283094b6ce286
unknown <you@example.com>
2024-04-03 08:22:20 +0200

    first commit

---
+++ projet-graine/src/main/java/fr/projetgraine/ProjetGraineApplication.java
@@ -0,0 +1,13 @@
+package fr.projetgraine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGraineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGraineApplication.class, args);
+	}
+
+}
---
+++ projet-graine/src/test/java/fr/projetgraine/ProjetGraineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projetgraine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGraineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
